fdfd2e974cab95c1c9cfa383c6b676f9c8e53722
sakovalchuk99 <sakovalchuk99@gmail.com>
2018-12-11 17:59:09 +0200

    text

---
+++ Archiver/MainForm.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Archiver
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_AddToArchive = new System.Windows.Forms.Button();
+            this.button_ExtractFromArchive = new System.Windows.Forms.Button();
+            this.button_Exit = new System.Windows.Forms.Button();
+            this.button_SelectFile = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label_Status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_AddToArchive
+            // 
+            this.button_AddToArchive.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button_AddToArchive.Enabled = false;
+            this.button_AddToArchive.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_AddToArchive.Font = new System.Drawing.Font("Georgia", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_AddToArchive.Location = new System.Drawing.Point(20, 100);
+            this.button_AddToArchive.Margin = new System.Windows.Forms.Padding(4);
+            this.button_AddToArchive.Name = "button_AddToArchive";
+            this.button_AddToArchive.Size = new System.Drawing.Size(196, 46);
+            this.button_AddToArchive.TabIndex = 0;
+            this.button_AddToArchive.Text = "ДОБАВИТЬ В АРХИВ";
+            this.button_AddToArchive.UseVisualStyleBackColor = false;
+            this.button_AddToArchive.Click += new System.EventHandler(this.button_AddToArchive_Click);
+            // 
+            // button_ExtractFromArchive
+            // 
+            this.button_ExtractFromArchive.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button_ExtractFromArchive.Enabled = false;
+            this.button_ExtractFromArchive.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_ExtractFromArchive.Font = new System.Drawing.Font("Georgia", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_ExtractFromArchive.Location = new System.Drawing.Point(20, 153);
+            this.button_ExtractFromArchive.Margin = new System.Windows.Forms.Padding(4);
+            this.button_ExtractFromArchive.Name = "button_ExtractFromArchive";
+            this.button_ExtractFromArchive.Size = new System.Drawing.Size(196, 46);
+            this.button_ExtractFromArchive.TabIndex = 1;
+            this.button_ExtractFromArchive.Text = "ИЗВЛЕЧЬ ИЗ АРХИВА";
+            this.button_ExtractFromArchive.UseVisualStyleBackColor = false;
+            this.button_ExtractFromArchive.Click += new System.EventHandler(this.button_ExtractFromArchive_Click);
+            // 
+            // button_Exit
+            // 
+            this.button_Exit.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button_Exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_Exit.Font = new System.Drawing.Font("Georgia", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_Exit.Location = new System.Drawing.Point(495, 197);
+            this.button_Exit.Margin = new System.Windows.Forms.Padding(4);
+            this.button_Exit.Name = "button_Exit";
+            this.button_Exit.Size = new System.Drawing.Size(196, 46);
+            this.button_Exit.TabIndex = 2;
+            this.button_Exit.Text = "ВЫХОД";
+            this.button_Exit.UseVisualStyleBackColor = false;
+            this.button_Exit.Click += new System.EventHandler(this.button_Exit_Click);
+            // 
+            // button_SelectFile
+            // 
+            this.button_SelectFile.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button_SelectFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_SelectFile.Font = new System.Drawing.Font("Georgia", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_SelectFile.Location = new System.Drawing.Point(20, 37);
+            this.button_SelectFile.Margin = new System.Windows.Forms.Padding(4);
+            this.button_SelectFile.Name = "button_SelectFile";
+            this.button_SelectFile.Size = new System.Drawing.Size(196, 46);
+            this.button_SelectFile.TabIndex = 5;
+            this.button_SelectFile.Text = "ВЫБРАТЬ ФАЙЛ";
+            this.button_SelectFile.UseVisualStyleBackColor = false;
+            this.button_SelectFile.Click += new System.EventHandler(this.button_SelectFile_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(225, 48);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(465, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog_Select_File";
+            // 
+            // label_Status
+            // 
+            this.label_Status.AutoSize = true;
+            this.label_Status.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_Status.ForeColor = System.Drawing.Color.Teal;
+            this.label_Status.Location = new System.Drawing.Point(520, 161);
+            this.label_Status.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_Status.Name = "label_Status";
+            this.label_Status.Size = new System.Drawing.Size(0, 22);
+            this.label_Status.TabIndex = 8;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(707, 262);
+            this.Controls.Add(this.label_Status);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button_SelectFile);
+            this.Controls.Add(this.button_Exit);
+            this.Controls.Add(this.button_ExtractFromArchive);
+            this.Controls.Add(this.button_AddToArchive);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_AddToArchive;
+        private System.Windows.Forms.Button button_ExtractFromArchive;
+        private System.Windows.Forms.Button button_Exit;
+        private System.Windows.Forms.Button button_SelectFile;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label_Status;
+    }
+}
+
---
+++ Archiver/MainForm.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Archiver
+{
+    public partial class MainForm : Form
+    {
+        public class c_FileToArchive
+        {
+            public String SelectedFilePath { get; set; }
+            public String SelectedFile { get; set; }
+        }
+        c_FileToArchive pc_FTA = new c_FileToArchive();
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void button_Exit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button_SelectFile_Click(object sender, EventArgs e)
+        {
+            label_Status.Text = "";
+            button_AddToArchive.Enabled = false;
+            button_ExtractFromArchive.Enabled = false;
+            openFileDialog1.InitialDirectory = Directory.GetCurrentDirectory();
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                pc_FTA.SelectedFilePath = openFileDialog1.FileName;
+                pc_FTA.SelectedFile = Path.GetFileName(openFileDialog1.FileName);
+                textBox1.Text = Path.GetFileName(openFileDialog1.FileName);
+                if (Path.GetExtension(pc_FTA.SelectedFilePath) == ".uda")
+                {
+                    button_ExtractFromArchive.Enabled = true;
+                }
+                else { button_AddToArchive.Enabled = true; }
+            }
+
+        }
+
+        private void button_AddToArchive_Click(object sender, EventArgs e)
+        {
+
+            FileStream lfs_ReadFile = new FileStream(pc_FTA.SelectedFilePath, FileMode.Open);
+            BinaryReader lbr = new BinaryReader(lfs_ReadFile);
+            Byte[] lba_inFA = lbr.ReadBytes((int)lfs_ReadFile.Length);
+            lbr.Close();
+            lfs_ReadFile.Close();
+
+            List<Byte> list_EncryptFirst = new List<Byte>();
+            Byte lb_Byte = 0;
+            Int32 li_Count = 1;
+            for (int i = 0, j = 1; i < lba_inFA.Length; i++, j++)
+            {
+                if (j != lba_inFA.Length)
+                {
+
+                    if (lba_inFA[i] == lba_inFA[j])
+                    {
+                        li_Count++;
+                    }
+
+
+                    if (lba_inFA[i] != lba_inFA[j])
+                    {
+                        lb_Byte = lba_inFA[i];
+                        list_EncryptFirst.Add((Byte)li_Count);
+                        list_EncryptFirst.Add(lb_Byte);
+                        li_Count = 1;
+                    }
+
+
+                    if (li_Count == 255)
+                    {
+                        lb_Byte = lba_inFA[i];
+                        list_EncryptFirst.Add((Byte)li_Count);
+                        list_EncryptFirst.Add(lb_Byte);
+                        li_Count = 0;
+                    }
+                }
+
+                else
+                {
+                    lb_Byte = lba_inFA[i];
+                    list_EncryptFirst.Add((Byte)li_Count);
+                    list_EncryptFirst.Add(lb_Byte);
+                    li_Count = 1;
+                }
+            }
+
+            List<Byte> list_EncryptTwo = new List<Byte>();
+            li_Count = 0;
+            Int32 li_MarkFirst = 0;
+            Boolean lb_FindMark = false;
+            for (int i = 0; i < list_EncryptFirst.Count; i += 2)
+            {
+                if (list_EncryptFirst[i] == 1)
+                {
+                    if (lb_FindMark == false)
+                    {
+                        lb_FindMark = true;
+                        li_MarkFirst = i + 1;
+                        list_EncryptTwo.Add(00);
+                    }
+
+                    li_Count++;
+                    if (li_Count == 255)
+                    {
+                        list_EncryptTwo.Add((Byte)li_Count);
+                        for (int x = 0; x < li_Count; x++)
+                        {
+                            list_EncryptTwo.Add(list_EncryptFirst[li_MarkFirst]);
+                            li_MarkFirst += 2;
+                        }
+                        lb_FindMark = false;
+                        li_Count = 0;
+                    }
+                }
+                if (list_EncryptFirst[i] != 1)
+                {
+                    if (lb_FindMark == true)
+                    {
+                        list_EncryptTwo.Add((Byte)li_Count);
+                        for (int x = 0; x < li_Count; x++)
+                        {
+                            list_EncryptTwo.Add(list_EncryptFirst[li_MarkFirst]);
+                            li_MarkFirst += 2;
+                        }
+                        lb_FindMark = false;
+                        li_Count = 0;
+                        i -= 2;
+                    }
+                    else
+                    {
+                        list_EncryptTwo.Add(list_EncryptFirst[i]);
+                        list_EncryptTwo.Add(list_EncryptFirst[i + 1]);
+                    }
+                }
+                if (i == list_EncryptFirst.Count - 2 & lb_FindMark == true)
+                {
+                    list_EncryptTwo.Add((Byte)li_Count);
+                    for (int x = 0; x < li_Count; x++)
+                    {
+                        list_EncryptTwo.Add(list_EncryptFirst[li_MarkFirst]);
+                        li_MarkFirst += 2;
+                    }
+                }
+            }
+            Byte[] lba_Extension = Encoding.Default.GetBytes(Path.GetExtension(pc_FTA.SelectedFile));
+            list_EncryptTwo.InsertRange(0, lba_Extension);
+            String ls_ArchiveFileName = Path.GetDirectoryName(pc_FTA.SelectedFilePath) + "\\" + Path.GetFileNameWithoutExtension(pc_FTA.SelectedFile) + ".uda";
+            FileStream lfs_WriteArchive = new FileStream(ls_ArchiveFileName, FileMode.Create);
+            Byte[] ba_Archive = new Byte[list_EncryptTwo.Count];
+            list_EncryptTwo.CopyTo(ba_Archive);
+            lfs_WriteArchive.Write(ba_Archive, 0, ba_Archive.Length);
+            lfs_WriteArchive.Close();
+            button_AddToArchive.Enabled = false;
+            label_Status.Text = "ВЫПОЛНЕНО";
+        }
+
+        private void button_ExtractFromArchive_Click(object sender, EventArgs e)
+        {
+            FileStream lfs_ReadArchive = new FileStream(pc_FTA.SelectedFilePath, FileMode.Open);
+            BinaryReader lbr_ReadArchive = new BinaryReader(lfs_ReadArchive);
+            Byte[] ba_Extension = lbr_ReadArchive.ReadBytes(4);
+            Byte[] ba_ArchivedFile = lbr_ReadArchive.ReadBytes((int)lfs_ReadArchive.Length);
+            lbr_ReadArchive.Close();
+            lfs_ReadArchive.Close();
+            List<Byte> list_Extract = new List<Byte>();
+            int m = 0;
+            while (m != ba_ArchivedFile.Length)
+            {
+                if (ba_ArchivedFile[m] == 00)
+                {
+                    Int32 j = m + 1;
+                    for (int i = 1; i < (int)ba_ArchivedFile[j] + 1; i++)
+                    {
+                        list_Extract.Add(ba_ArchivedFile[j + i]);
+                    }
+                    m = m + (int)ba_ArchivedFile[j] + 2;
+                }
+                else
+                {
+                    for (int i = 0; i < (int)ba_ArchivedFile[m]; i++)
+                    {
+                        list_Extract.Add(ba_ArchivedFile[m + 1]);
+                    }
+                    m += 2;
+                }
+            }
+
+            String ls_Extension = Encoding.Default.GetString(ba_Extension);
+            String ls_ExtractFileName = Path.GetDirectoryName(pc_FTA.SelectedFilePath) + "\\" + Path.GetFileNameWithoutExtension(pc_FTA.SelectedFile) + "extr" + ls_Extension;
+            FileStream lfs_WriteArchive = new FileStream(ls_ExtractFileName, FileMode.Create);
+            Byte[] ba_Extract = new Byte[list_Extract.Count];
+            list_Extract.CopyTo(ba_Extract);
+            lfs_WriteArchive.Write(ba_Extract, 0, ba_Extract.Length);
+            lfs_WriteArchive.Close();
+            button_ExtractFromArchive.Enabled = false;
+            label_Status.Text = "ВЫПОЛНЕНО";
+        }
+
+    }
+}
